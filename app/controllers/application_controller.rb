89e0c0d9a3ff9746a1d57a006c89c64d14b713af
増渕佳輝 <yoshxj4@gmail.com>
2017-02-13 07:57:52 +0900

    add devisePermitsKey update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:rname])
   end
 end
