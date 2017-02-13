27a5a0f4bd63ffa1c5b12074ba418045e909a753
増渕佳輝 <yoshxj4@gmail.com>
2017-02-13 17:48:42 +0900

    import group.scss,set routing,set model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'chat_groups#index'
-  get '/chat_groups/new' , 'chat_groups#new'
-  get '/chat_groups/:id/edit' , 'chat_groups#edit'
+  resources :chat_groups, except: [:show, :destroy]
 end
