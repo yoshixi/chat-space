85b76de3780bd781fab5fa9664c33fcdcce4fefa
増渕佳輝 <yoshxj4@gmail.com>
2017-02-13 14:37:20 +0900

    rm varidates :email and add partial template

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, :email, presence: true
+  validates :name,  presence: true
 end
