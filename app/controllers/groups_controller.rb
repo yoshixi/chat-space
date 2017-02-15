06e7fc13e0b1faa110623828ac06c4a1328ccb10
増渕佳輝 <yoshxj4@gmail.com>
2017-02-15 10:13:35 +0900

    implement create groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,7 +5,17 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+  def create
+    @group =  Group.new(group_params)
+    @group.save
+  end
   def edit
     #code
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
