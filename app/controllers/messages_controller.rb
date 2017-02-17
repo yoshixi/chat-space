d024ebcdbd2cb9555608c36e4021feb69f96e133
増渕佳輝 <yoshxj4@gmail.com>
2017-02-17 17:23:15 +0900

    set actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,21 @@
 class MessagesController < ApplicationController
+  def index
+    @groups = current_user.groups
+    @group = Group.find(params[:group_id])
+    @message =  Message.new
+    @messages = current_user.messages
+    @members = @group.users
+  end
+
+  def create
+
+    Message.create(create_params)
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def create_params
+    params.require(:message).permit(:text,:image).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
 end
