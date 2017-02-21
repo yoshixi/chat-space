ce565ca7618ec1466da689f7b944c44654597bdd
増渕佳輝 <yoshxj4@gmail.com>
2017-02-21 12:41:17 +0900

    modify

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -6,8 +6,9 @@ describe MessagesController do
   let(:group) { create(:group) }
   let(:message) { create(:message, user_id: user.id, group_id: group.id) }
   let(:messages) { create_list(:message, 4,user: user, group: group)}
-
-
+  before do
+    sign_in user
+  end
   describe 'GET #index' do
     before do
       login_user user
@@ -19,6 +20,9 @@ describe MessagesController do
     it "assigns the requested contact to @group" do
       expect(assigns(:group)).to eq group
     end
+    it "assigns the requested contact to @messages" do
+      expect(assigns(:messages)).to eq messages
+    end
     it "renders the :index template" do
       expect(response).to render_template :index
     end
@@ -28,10 +32,18 @@ describe MessagesController do
     it "saves message in the database" do
       expect(message).to be_valid
     end
+    it "saves message in the database and redirect_to index" do
+      post :create, group_id: group, message: attributes_for(:message, group_id: group.id, user_id: user.id)
+      expect(response).to redirect_to group_messages_path
+    end
     it "not save messaege in the database" do
       message = build(:message, text: nil)
       message.valid?
       expect(message.errors[:text]).to include("を入力してください。")
+    end
+    it "not save messaege in the database and redirect_to index_" do
+      post :create, group_id: group, message: attributes_for(:message,text: "", group_id: group.id, user_id: user.id)
+      expect(response).to redirect_to group_messages_path
     end
   end
 
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,7 +4,7 @@ require 'support/controller_macros'
 describe Message do
   describe '#create' do
     it "is invalid without a text" do
-	     message  = build(:message, text: "")
+	     message = build(:message, text: "")
 	     message.valid?
 	     expect(message.errors[:text]).to include("を入力してください。")
     end
