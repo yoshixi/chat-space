1e035904979a0f8c0e925a5e397ac695325a7eaa
増渕佳輝 <yoshxj4@gmail.com>
2017-02-24 20:06:57 +0900

    wasureteta

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,12 +9,16 @@ class MessagesController < ApplicationController
 
   def create
     @message = current_user.messages.new(message_params)
-    if !@message.save
-      @message.errors.full_messages.each do |e|
-        flash[:notice] = e
+    data = {name: @message.user.name, time: @message.created_at, text: @message.text}
+    if @message.save
+      respond_to do |format|
+        format.json { render json: data }
+        format.html { redirect_to action: 'index' }
       end
+    else
+      @message.errors.full_messages.each{ |e| flash[:notice] = e}
+      redirect_to action: 'index'
     end
-    redirect_to action: 'index'
   end
 
   private
