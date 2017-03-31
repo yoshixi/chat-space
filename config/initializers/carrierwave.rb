38318c7eb51ac15630f4f66fb51ef58405358859
増渕佳輝 <yoshxj4@gmail.com>
2017-03-31 16:47:02 +0900

    modify config -e production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,8 +9,8 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = 'dummy'
-      config.asset_host =   'https://s3-ap-northeast-1.amazonaws.com/test.dummy'
+      config.fog_directory = '201703chatspace'
+      config.asset_host =  'http://201703chatspace.s3-website-ap-northeast-1.amazonaws.com'
 
     when 'development'
       config.fog_directory = '201703chatspace'
